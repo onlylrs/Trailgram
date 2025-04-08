78fe695209cd60d3244bf4dac9f02b400b23005e
runsheng <liurunsheng0507@gmail.com>
2025-04-08 13:23:45 -0700

    Initial Commit

---
+++ Trailgram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trailgram
+//
+//  Created by 刘闰生 on 8/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trailgram/TrailgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailgramApp.swift
+//  Trailgram
+//
+//  Created by 刘闰生 on 8/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrailgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
